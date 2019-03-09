b6f8d4d64c5d1f7da9c0cfb9244b3f57354f171e
IceFox <zengxinhai001@163.com>
2019-03-09 20:23:36 +0800

    Move code to src folder

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ Aim of this practice:
 1. Autoupdate the page when I change the corresponding variables in the code
 2. The mark attribute would be data.
 */
+import zxh from './lib';
 
 zxh.register('time');
 
-var refreshNameInterval = setInterval(function(i) {
+var refreshNameInterval = setInterval(() => {
   zxh.setState('time', Date.now());
 }, 1000);
 
-setTimeout(function() {
+setTimeout(() => {
   clearInterval(refreshNameInterval);
 }, 10000);
